98e503df909aa6a00de4a2a44989312a9effcfa1
dapinglee <ldp01@vip.qq.com>
2019-12-07 15:51:09 +0800

    add staying online

--- cuit.py
+++ cuit.py
@@ -5,36 +5,81 @@ Created on 2019/12/5 005 下午 4:13
 @license : Copyright(C), CUIT
 @contact : ldp01@vip.qq.com
 """
+import json
 import sys
+import time
 import requests
 
 
-def get_info():
-    headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Mobile Safari/537.36'}
-    ret = requests.get('http://10.254.241.19/', headers=headers)
-    return ret.text.split('\'')[1].split('?')[1]
+class Connect:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.url = 'http://10.254.241.19/'
+        self.login_url = 'http://10.254.241.19/eportal/InterFace.do?method=login'
+        self.logout_url = 'http://10.254.241.19:8080/eportal/interface/index_files/pc/login_bch.js'
+        self.online_info_url = 'http://10.254.241.19/eportal/InterFace.do?method=getOnlineUserInfo'
+        self.web_rul = 'https://www.baidu.com/'
+        self.user_agent = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Mobile Safari/537.36'
+        self.userIndex = None
 
+    def is_online(self):
+        headers = {'User-Agent': self.user_agent}
+        data = {'userIndex': self.userIndex}
+        ret = requests.post(self.online_info_url, headers=headers, data=data)
+        ret.encoding = ret.apparent_encoding
+        ret2dic = json.loads(ret.content)
+        if ret2dic['result'] == 'fail':
+            return False
+        else:
+            return True
 
-def login(username, password):
-    headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Mobile Safari/537.36'}
-    data = {'userId': username,
-            'password': password,
-            'service': '%E6%A0%A1%E5%9B%AD%E7%BD%91',
-            'queryString': get_info(),
-            'operatorPwd': '',
-            'operatorUserId': '',
-            'validcode': '',
-            'passwordEncrypt': 'false'}
-    ret = requests.post('http://10.254.241.19/eportal/InterFace.do?method=login', headers=headers, data=data)
+    def get_info(self):
+        headers = {'User-Agent': self.user_agent}
+        ret = requests.get(self.url, headers=headers)
+        return ret.text.split('\'')[1].split('?')[1]
 
-    if 'success' in ret.text:
-        print('登录成功，可关闭程序')
-    else:
-        print('登录失败，请检查用户名和密码是否正确')
+    def login(self):
+        headers = {'User-Agent': self.user_agent}
+        data = {'userId': self.username,
+                'password': self.password,
+                'service': '%E6%A0%A1%E5%9B%AD%E7%BD%91',
+                'queryString': self.get_info(),
+                'operatorPwd': '',
+                'operatorUserId': '',
+                'validcode': '',
+                'passwordEncrypt': 'false'}
+        ret = requests.post(self.login_url, headers=headers, data=data)
+        ret.encoding = ret.apparent_encoding
+        ret2dic = json.loads(ret.content)
+
+        if ret2dic['result'] == 'success':
+            print('登录成功')
+        else:
+            print('登录失败')
+            print(ret2dic['message'])
+            sys.exit(1)
+
+    def stay_online(self):
+        print('发送心跳包，维持在线中...')
+        while True:
+            # 每隔10秒发送心跳包
+            time.sleep(10)
+            if not self.is_online():
+                print('检测到可能掉线，正在重新登录...')
+                self.login()
+                print('发送心跳包，维持在线中...')
 
 
 if __name__ == '__main__':
-    try:
-        login(sys.argv[1], sys.argv[2])
-    except:
-        print('登录失败，请检查是否已经在线')
+    if len(sys.argv) != 3:
+        print('用法: python {} username password'.format(sys.argv[0].split('/')[-1]))
+        sys.exit(1)
+
+    cuit = Connect(sys.argv[1], sys.argv[2])
+    if not cuit.is_online():
+        cuit.login()
+        cuit.stay_online()
+    else:
+        print('用户已经在线，无需登录')
+        cuit.stay_online()
